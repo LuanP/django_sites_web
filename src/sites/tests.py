01ae7e4170a673666bdcbcc440fab17f8096e230
Luan Pablo <luanpab@gmail.com>
2018-07-07 09:15:56 -0300

    Add sites fixtures

--- src/sites/tests.py
+++ src/sites/tests.py
@@ -3,22 +3,29 @@ from django.test import TestCase, Client
 
 
 class TestSite(TestCase):
+    fixtures = ['sites.json']
+
     def setUp(self):
         self.client = Client()
 
     def test_home_site_list(self):
         sites_list = self.client.get(reverse('sites:home_list'))
 
-        self.assertTrue(sites_list.status_code, 200)
+        self.assertEqual(sites_list.status_code, 200)
         self.assertTemplateUsed(sites_list, 'sites/list.html')
 
     def test_sites_list(self):
         sites_list = self.client.get(reverse('sites:list'))
 
-        self.assertTrue(sites_list.status_code, 200)
+        self.assertEqual(sites_list.status_code, 200)
         self.assertTemplateUsed(sites_list, 'sites/list.html')
 
-    def test_site_not_found(self):
+    def test_site_detail_not_found(self):
         sites_list = self.client.get(reverse('sites:detail', kwargs={'id': 9999}))
 
-        self.assertTrue(sites_list.status_code, 404)
+        self.assertEqual(sites_list.status_code, 404)
+
+    def test_site_detail(self):
+        sites_list = self.client.get(reverse('sites:detail', kwargs={'id': 1}))
+
+        self.assertEqual(sites_list.status_code, 200)
